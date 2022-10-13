a3cd1b10946fca584fde8a87cd8d1603ad8b3a86
Ram Sankar <RamSankarTheDeveloper@gmail.com>
2022-10-14 00:24:04 +0530

    make a class to make thread await

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import tkinter
 import threading
+import winsound
+import asyncio
 wndw = tkinter.Tk()
 lbl = tkinter.Label(
     text="Hello, Tkinter",
@@ -9,12 +11,21 @@ lbl = tkinter.Label(
     height=2
 )
 
-def startcount():
-    print("nothing")
+def waitThis():
+    frequency = 1000 # Set Frequency To 2500 Hertz
+    duration = 200  # Set Duration To 1000 ms == 1 second
+    winsound.PlaySound("SystemQuestion", winsound.SND_ALIAS)
+class customthread:
+    def __init__(self):
+        threading.Timer(1500, waitThis)
+async def startaction():
+    pomodorotimer=threading.Timer(1500,waitThis)
+    shortbreaktimer=threading.Timer(300,waitThis)
+    longbreaktimer=threading.Timer(1200,waitThis)
+    a= pomodorotimer.start()
+    lbl.config(font=('Helvatical bold',20))
 
-lbl.config(font=('Helvatical bold',20))
-
-strtbtn= tkinter.Button(wndw, text="20", command= startcount)
+strtbtn= tkinter.Button(wndw, text="20", command= startaction)
 
 
 lbl.pack()
