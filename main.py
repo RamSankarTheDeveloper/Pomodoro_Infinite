e09c7eca49b8a85a19efe22d5d974f208b44129a
Ram Sankar <RamSankarTheDeveloper@gmail.com>
2022-10-13 22:48:00 +0530

    basic executable window

--- main.py
+++ main.py
@@ -8,23 +8,15 @@ lbl = tkinter.Label(
     width=20,
     height=2
 )
-i=0
-j=0
+
 def startcount():
-    global i,j
-    while(i<2):
-        i=i+1
-        wndw.after(600,startcount())
-        j=j+1
-        def startcountbreak():
-            wndw.after(6000, startcount())
+    print("nothing")
 
 lbl.config(font=('Helvatical bold',20))
 
 strtbtn= tkinter.Button(wndw, text="20", command= startcount)
-strtbtn.pack(pady=10)
-#stpbtn= tkinter.button(wndw, text="stop", command=stopcount())
-#stpbtn.pack(pady=10)
+
 
 lbl.pack()
+strtbtn.pack()
 wndw.mainloop()
