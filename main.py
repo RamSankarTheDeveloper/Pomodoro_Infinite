66eb7bfac052c9fad6949f56b6530828012cee27
Ram Sankar <RamSankarTheDeveloper@gmail.com>
2022-10-14 11:05:14 +0530

    mvp for testing

--- main.py
+++ main.py
@@ -12,24 +12,34 @@ lbl = tkinter.Label(
 )
 
 a = 0
-import threading
 def pomodoro():
+
+    winsound.PlaySound("SystemExit", winsound.SND_ALIAS)
     global a
     a = a+1
+
     if ((a%8)==0):
         b=threading.Timer(3,pomodoro)
+        b.daemon=True
         print('longbreak')
         b.start()
     elif ((a%2)==0):
         b=threading.Timer(3,pomodoro)
+        b.daemon = True
         print('shortbreak')
         b.start()
     else:
+        lbl.config(text=int((a/2)+.5) )#just an eqn to subract the no. of breaks from global variable
         b=threading.Timer(3,pomodoro)
+        b.daemon = True #daemon-threads gets killed when sys.exit()
         print('pomodoro')
         b.start()
-
-strtbtn= tkinter.Button(wndw, text="20", command= pomodoro)
+#MVP
+#primary:make a non daemon thread to store data if window is closed
+#make string along with number
+#make a custom louder sound that supports everywhere
+#secondary:make a stop function to kill the daemon pomodoro thread without closing window
+strtbtn= tkinter.Button(wndw, text="Start", command= pomodoro)
 
 
 lbl.pack()
